c3251b6c51e481bcc6f55b30ef3e0d10480f5143
Alexis Mandracchia <alexis.mandracchia@diveria.com>
2020-09-03 12:31:24 -0300

    home y menu agregados

--- Desarrollo/website/src/components/AppNavigation.vue
+++ Desarrollo/website/src/components/AppNavigation.vue
@@ -14,12 +14,37 @@
         </v-navigation-drawer>
         <v-toolbar app color="white" dark>
             <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
-                <img alt="Vue logo" width="30px;" src="../assets/logo.png">
-                <v-spacer class="hidden-md-and-up"></v-spacer>
-                <router-link to="/">
-                    <v-toolbar-title style="color: black; margin-left:20px;">Inicio</v-toolbar-title>
-                </router-link>
-                <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <img alt="Vue logo" width="30px;" src="../assets/logo.png">
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <router-link to="/">
+                <v-toolbar-title style="color: black; margin-left:20px;">Inicio</v-toolbar-title>
+            </router-link>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <div class="text-center">
+                <v-menu offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                    color="primary"
+                    dark
+                    v-bind="attrs"
+                    v-on="on"
+                    class="menu label"
+                    >
+                    Ver Horarios
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item
+                    v-for="(item, index) in items"
+                    :key="index"
+                    @click="item.url"
+                    >
+                    <v-list-item-title class="label-menu"><router-link :to="item.url">{{ item.title }}</router-link></v-list-item-title>
+                    </v-list-item>
+                </v-list>
+                </v-menu>
+            </div>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
             <v-btn flat class="hidden-sm-and-down" color="orange">SALIR</v-btn>
         </v-toolbar>
     </span>
@@ -33,9 +58,9 @@ export default {
             appTitle: 'Website Horarios',
             drawer: false,
             items: [
-                { title: 'Menu' },
-                { title: 'Sign In' },
-                { title: 'Join' }
+                { title: 'Mañana', url: '/maniana'},
+                { title: 'Taller', url: '/tarde' },
+                { title: 'Edu. Física', url: '/edu-fisica' }
             ]
         };
     }
@@ -43,4 +68,13 @@ export default {
 </script>
 
 <style scoped>
+.label-menu{
+    background: transparent;
+    border-radius: 10px;
+    font-size: 16px;
+}
+a {
+    color: blue;
+    text-decoration: none;
+}
 </style>
--- Desarrollo/website/src/views/Home.vue
+++ Desarrollo/website/src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="home">
+    <div class="titulo">
+      E.E.S.T.P.P.I. Nº 8122
+      <br>
+      "Nuestra Señora de Lourdes"
+    </div>
+    <div class="subtitulo">
+      Sitio Web Horarios 2020
+    </div>
+    <div class="anuncios">
+      <div class="anuncios-titulo">
+        Anuncios
+      </div>
+      <div class="anuncio-content">
+        No hay anuncios correspondientes
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +35,36 @@ export default {
 .home{
   background-color: #f2f2f2;
   height: 100%;
+  text-align: center;
+  font-family: 'Public Sans';
+  padding-top: 5%;
+}
+
+.titulo{
+  font-size: 40px;
+  font-weight: 700;
+  padding: 20px;
+}
+
+.subtitulo{
+  font-size: 32px;
+  font-weight: 500;
+  padding: 20px;
+}
+
+.anuncios{
+  padding: 25px;
+}
+
+.anuncios-titulo{
+  font-size: 24px;
+  font-weight: 300;
+}
+
+.anuncio-content{
+  font-style: italic;
+  color: green;
+  font-weight: 700;
+  font-size: 20px;
 }
 </style>
